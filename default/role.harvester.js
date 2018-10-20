345fdbcdfee0992f88c6a1d9a9ddd76099e2aca5
schneider <bastian.Schneider@hs-furtwangen.de>
2018-10-20 16:28:42 +0200

    zeug

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -33,6 +33,7 @@ var roleHarvester = {
                 targets = creep.room.find(FIND_FLAGS);
                 creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
+            console.log('hallo');
         }
 	}
 };
